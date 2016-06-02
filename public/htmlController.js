b19028c82990c57a00c6474bf40d70fc84038b59
mkolkowski <marius.kolkowski@esemos.de>
2016-06-02 18:37:51 +0200

    - add rich html editor

--- public/htmlController.js
+++ public/htmlController.js
@@ -1,5 +1,12 @@
 define(function (require) {
-  var module = require('ui/modules').get('kibana/kibana-html-plugin', ['kibana']);
+  var module = require('ui/modules').get('kibana/kibana-html-plugin', ['kibana', 'ui.ace']);
+
+  module.controller('KbnHtmlEditController', ['$scope', function($scope) {
+    $scope.aceLoaded = function(_editor){
+      _editor.$blockScrolling = Infinity;
+    };
+  }]);
+
   module.controller('KbnHtmlVisController', function ($scope, $sce) {
     $scope.$watch('vis.params.html', function (html) {
       if (!html) return;
